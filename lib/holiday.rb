0a52e8da1b4282c5c83636923da5d747fea3815e
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-19 18:00:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,6 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
     value.each do |key2, value2|
     
     puts "#{key.capitalize}:" 
+    binding.pry
                 puts "#{key2.capitalize}: #{value2}"  
     end
   end

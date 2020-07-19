1a3732e39083e6f8d69e1650b91ef8aaa985bb5b
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-19 18:12:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,9 +61,9 @@ end
 def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |season, holidays|
-    holidays.each do |holiday, supplies|
+    puts "#{season.capitalize}:"  holidays.each do |holiday, supplies|
     binding.pry
-    puts "#{season.capitalize}:" 
+   
     
                 puts "#{holiday.capitalize}: #{supplies}"  
     end
